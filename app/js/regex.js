a2b11dfd4362de2c4d6a739b162e75b7627d4019
Robbie Wood <githubphagocyte@gmail.com>
2017-10-27 16:43:17 +0100

    Reference form by class instead of id to match html following review.

--- app/js/regex.js
+++ app/js/regex.js
@@ -1,13 +1,13 @@
 //function to validate the email address using RegEx
 //takes the ID of the e-mail input filed and check if the e-mail content is valid
 
-document.getElementById('container_page').addEventListener('submit', function (event) {
+document.getElementsByClassName('container_page')[0].addEventListener('submit', function(event) {
     var regexEmail = /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/
-    var email = document.getElementById("e-mail")
+    var email = document.getElementById("email")
     if (regexEmail.test(email.value)) {
         return true
     } else {
-        document.getElementById("e-mail").insertAdjacentHTML('afterend', "<span class='title_input'>Please provide valid email</span>")
+        document.getElementById("email").insertAdjacentHTML('afterend', "<span class='title_input'>Please provide valid email</span>")
         event.preventDefault()
         event.returnValue = false
         return false

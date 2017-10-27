f17cd897b5d031c08acb61306ad7b9f53c83d5ac
Peter Kanev <pdkanew1@gmail.com>
2017-10-27 14:48:10 +0100

    function to valide e-mail through Regex fixed functionality

--- app/js/regex.js
+++ app/js/regex.js
@@ -1,16 +1,15 @@
 //function to validate the email address using RegEx
 //takes the ID of the e-mail input filed and check if the e-mail content is valid
 
-$('#myForm').on('submit', function () {
-
+document.getElementById('container_page').addEventListener('submit', function (event) {
     var regexEmail = /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/
     var email = document.getElementById("e-mail")
-
     if (regexEmail.test(email.value)) {
         return true
-
     } else {
         document.getElementById("e-mail").insertAdjacentHTML('afterend', "<span class='title_input'>Please provide valid email</span>")
+        event.preventDefault()
+        event.returnValue = false
         return false
     }
 })

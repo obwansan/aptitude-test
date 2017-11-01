8223bf10295193e8da5a83fdd266f333c03c9ec6
Ben Long <bennlongg@gmail.com>
2017-11-01 09:20:49 +0000

    deleted redundent files and make pull request changes

--- app/js/ajaxHandlebarsTemplate.js
+++ app/js/ajaxHandlebarsTemplate.js
@@ -1,15 +1,15 @@
 function getTemplateAjax(path, callback) {
-    var source;
-    var template;
+    var source
+    var template
 
     $.ajax({
         url: path,
         success: function (data) {
-            source = data;
-            template = Handlebars.compile(source);
+            source = data
+            template = Handlebars.compile(source)
 
             //execute the callback if passed
-            if (callback) callback(template);
+            if (callback) callback(template)
         }
-    });
+    })
 }

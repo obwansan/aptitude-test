1516e3e05d2303a443d35c78657a6985d283d833
Robbie Wood <githubphagocyte@gmail.com>
2017-11-01 11:46:48 +0000

    Adjust addNewUser.js to use fetch now we are using ES6.

--- app/js/addNewUser.js
+++ app/js/addNewUser.js
@@ -1,20 +1,38 @@
 /**
- * saves the stringified JSON object using an AJAX request
+ * Save the JSON object using an AJAX request
  *
- * @param user The stringified JSON object
+ * @param user The JSON object including name and email keys
  *
- * @returns {boolean} Was the user successfully saved?
+ * @returns A promise containing the response, which includes the boolean success property
  */
-function saveNewUser(user) {
-    var result = false
-    var http = new XMLHttpRequest()
-    http.open('POST', 'http://localhost:8080/user', true)
+async function saveNewUser(user) {
+    formData = jsonToFormData(user) // API does not work with JSON - needs form data.
 
-    http.addEventListener('load', function () {
-        var resultingData = JSON.parse(this.responseText)
-        result = resultingData.success
+    let apiData = await fetch(
+        'http://localhost:8080/user',
+        {
+            method: 'POST',
+            body: formData
+        }
+    )
+
+    apiData = await apiData.json()
+    return apiData
+}
+
+/**
+ * Convert a JSON object into form data suitable for passing to an API built for form data.
+ *
+ * @param jsonInput The JSON object to be converted.
+ *
+ * @returns A form data object.
+ */
+function jsonToFormData(jsonInput) {
+    let formData = new FormData()
+
+    Object.keys(jsonInput).forEach((key) => {
+        formData.append(key, jsonInput[key])
     })
-
-    http.send(user)
-    return result
+    
+    return formData
 }

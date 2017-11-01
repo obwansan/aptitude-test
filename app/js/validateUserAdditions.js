36ad56660903aea9da98b8a085efc0efba7c1951
Ismael Velasco <mayel.vel@gmail.com>
2017-11-01 10:51:59 +0000

    removed floating function calls + edited prevent function

--- app/js/validateUserAdditions.js
+++ app/js/validateUserAdditions.js
@@ -1,6 +1,5 @@
-function isEmailValid() {
+function isEmailValid(email) {
         var regexEmail = /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/
-        var email = document.getElementById("email")
         if (regexEmail.test(email.value)) {
             return true
         } else {
@@ -8,19 +7,80 @@ function isEmailValid() {
         }
 }
 
-isEmailValid()
 
-function preventDefault() {
-    document.getElementsByClassName('container_controls')[0].addEventListener('submit', function (event) {
-        if (isEmailValid() !== true) {
-            document.getElementById("email").insertAdjacentHTML('afterend', "<span class='title_input'>Please provide valid email</span>")
-            event.preventDefault()
-            event.returnValue = false
+
+/**
+ *  result is set to false unless the email matches an existing user's email.
+ *
+ * @param emailToAdd  - The email address we want to check for
+ * @param existingUsers - The array of existing users data
+ *
+ * @returns {boolean} - Does the user already exist
+ */
+function doesUserExist(emailToAdd, existingUsers) {
+    var result = false
+    existingUsers.forEach(function(user) {
+        if (user.email === emailToAdd) {
+            result = true
         }
     })
+    return result
 }
-preventDefault()
 
+
+document.getElementsByClassName('container_controls')[0].addEventListener('submit', function(event) {
+    // THIS IS DUMMY DATA
+    var existingUsers = [
+        {
+            "id": "1",
+            "email": "emailme@mikeoram.co.uk",
+            "name": "Mike",
+            "dateCreated": "2017-10-26 13:48:29",
+            "isAdmin": "1",
+            "deleted": "0"
+        }, {
+            "id": "2",
+            "email": "emailme@mikeTram.co.uk",
+            "name": "M2e",
+            "dateCreated": "2017-11-26 13:48:29",
+            "isAdmin": "1",
+            "deleted": "0"
+        }
+    ]
+    var email = document.getElementById("email")
+    if (isEmailValid(email) !== true) {
+        document.getElementById("email").insertAdjacentHTML('afterend', "<div class='title_input'>Your email is not valid or already exists: Please provide a correct email</div>")
+        event.preventDefault()
+        event.returnValue = false
+    }
+})
+
+
+//create event listener submit
+// prevent default
+// call is email valid
+// define emailToAdd + existingUsers
+// does user exist
+
+
+// THIS IS DUMMY DATA
+// var existingUsers = [
+//     {
+//         "id": "1",
+//         "email": "emailme@mikeoram.co.uk",
+//         "name": "Mike",
+//         "dateCreated": "2017-10-26 13:48:29",
+//         "isAdmin": "1",
+//         "deleted": "0"
+//     }, {
+//         "id": "2",
+//         "email": "emailme@mikeTram.co.uk",
+//         "name": "M2e",
+//         "dateCreated": "2017-11-26 13:48:29",
+//         "isAdmin": "1",
+//         "deleted": "0"
+//     }
+// ]
 
 // isEmailValid()
 // function doesUserExist(emailToAdd, existingUsers) {

4710b571c5ceb92a6df5c27e22d424fbf698f363
Vasyl Kozhushko <kozhushko.vasyl@gmail.com>
2017-11-02 10:41:41 +0000

    added variable for score

--- app/js/score_result.js
+++ app/js/score_result.js
@@ -1,6 +1,10 @@
-
-function result() {
-    let result = 25 //number of points
+/**
+ * showing and calculating result in points and percents
+ *
+ * @param earnedPoints total amount of right questions
+ */
+function result(earnedPoints) {
+    let result = earnedPoints //number of points
     let questionAmmount = 30 //ammount of questions
     document.querySelector(".score").innerHTML = result
     document.querySelector(".score_per").innerHTML = Math.round(result / questionAmmount * 100)

f012aadd0a3121f1999d89d2c1de7b41de827171
Vasyl Kozhushko <kozhushko.vasyl@gmail.com>
2017-11-02 10:54:39 +0000

    cheanged class name

--- app/js/score_result.js
+++ app/js/score_result.js
@@ -7,7 +7,7 @@ function result(earnedPoints) {
     let result = earnedPoints //number of points
     let questionAmount = 30 //ammount of questions
     document.querySelector(".score").innerHTML = result
-    document.querySelector(".score_per").innerHTML = Math.round(result / questionAmount * 100)
+    document.querySelector(".score_percentage").innerHTML = Math.round(result / questionAmount * 100)
 }
 
-result()
+result(23)

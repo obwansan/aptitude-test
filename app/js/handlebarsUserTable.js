620e6765a93481e2a37b0ed41a4e994d20ee0ade
Peter Kanev <pdkanew1@gmail.com>
2017-11-02 13:46:07 +0000

    fix the consistency issue with handelbarsUserTable.js

--- app/js/handlebarsUserTable.js
+++ app/js/handlebarsUserTable.js
@@ -1,10 +1,10 @@
 /**
  * fills handlebars template by getting the user data from the api and inserts into the user_list div
  *
- * @param HBTemplate the handlebars template
+ * @param hbTemplate the handlebars template
  */
-function fillUserTable(HBTemplate) {
-    let template = Handlebars.compile(HBTemplate)
+function fillUserTable(hbTemplate) {
+    let template = Handlebars.compile(hbTemplate)
 
     fetch("http://localhost:8080/user")
         .then(function (result) {
@@ -29,8 +29,8 @@ function fillUserTable(HBTemplate) {
  * get the handlebars template and use this to display the users
  */
 function updateUserTable() {
-    getTemplateAjax('js/templates/userTable.hbs').then(function (HBTemplate) {
-        fillUserTable(HBTemplate)
+    getTemplateAjax('js/templates/userTable.hbs').then(function (hbTemplate) {
+        fillUserTable(hbTemplate)
     })
 }
 

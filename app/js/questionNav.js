5261806131bb1199549f87c00991d85e62bdd8db
Ben Long <bennlongg@gmail.com>
2017-11-02 11:52:24 +0000

    more querys to hide buttons

--- app/js/questionNav.js
+++ app/js/questionNav.js
@@ -1,27 +1,51 @@
-selected = document.querySelectorAll(".question div")[0]
-selected.classList.add("active")
-
-
+var current = 1
+/*
+*adds active to the first question
+* function in called when questions are inserted into html
+ */
+function active() {
+    document.querySelector(".q_" + current).classList.add("active")
+}
+/*
+*adds active class to next question, removes from current question
+* makes the prev and next buttons appear when needed
+ */
 function next() {
+    current++
+    let nextQuestion = document.querySelector(".q_" + current)
 
-    let currentQuestion = document.querySelector(".active")
-    let nextQuestion = currentQuestion.nextElementSibling
-
-    currentQuestion.classList.remove("active")
-    nextQuestion.classList.add("active")
-
+    if(current !== 1) {
+        document.querySelector(".prev").style.display = "block"
+    }
+    if(current === 30) {
+        document.querySelector(".next").style.display = "none"
+    }
+    if (nextQuestion !== null) {
+        document.querySelector(".active").classList.remove("active")
+        document.querySelector(".q_" + current).classList.add("active")
+    }
 }
 
-
+/*
+*adds active class to prev question, removes from current question
+* makes the prev and next buttons appear when needed
+ */
 function prev() {
+    current--
+    let prevQuestion = document.querySelector(".q_" + current)
 
-    let currentQuestion = document.querySelector(".active")
-    let prevQuestion = currentQuestion.previousElementSibling
-
-    currentQuestion.classList.remove("active")
-    prevQuestion.classList.add("active")
+    if(current === 1) {
+        document.querySelector(".prev").style.display = "none"
+        document.querySelector(".next").style.display = "block"
+    }
+    if(current !== 30) {
+        document.querySelector(".next").style.display = "block"
+    }
+    if (prevQuestion !== null) {
+        document.querySelector(".active").classList.remove("active")
+        document.querySelector(".q_" + current).classList.add("active")
+    }
 }
 
-
-document.querySelector(".nextButton").addEventListener("onClick", next)
-document.querySelector(".prevButton").addEventListener("onClick", prev)
+document.querySelector(".next").addEventListener("click", next)
+document.querySelector(".prev").addEventListener("click", prev)

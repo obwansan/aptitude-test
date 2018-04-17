ad4ce445f535a631b168d798b2cca7bff45d6383
Karim Kefi <karimkefi@gmail.com>
2018-04-17 16:27:42 +0100

    Object created to combine user information and user results

--- app/js/checkScores.js
+++ app/js/checkScores.js
@@ -5,7 +5,7 @@ const numberOfQuestions = 30
  *
  * @return Promise - containing the score results
  */
-async function getScores() {
+async function applyPercent() {
     let scoreObj = await fetch("http://localhost:8080/result", {method: 'get'})
         .then(function (data) {
             return data.json()
@@ -13,12 +13,64 @@ async function getScores() {
         .then(function (data) {
             return data
         })
-
     scoreObj.data.forEach(function (element) {
         element.percent = (element.score / numberOfQuestions * 100).toFixed(2)
     })
     return scoreObj.data
 }
 
-//this can be removed at a later date...
-console.log(getScores())
+/**
+ * gets all users name and email from api
+ *
+ * @return Promise - containing the user info
+ */
+async function getNameAndEmail() {
+    let userObj = await fetch("http://localhost:8080/user", {method: 'get'})
+        .then(function (data) {
+            return data.json()
+        })
+        .then(function (data) {
+            return data
+        })
+
+    let userArray = []
+    userObj.data.forEach(function(element) {
+        let obj = {}
+        let {id, email, name} = element
+        obj['id'] = id
+        obj['name'] = name
+        obj['email'] = email
+        userArray.push(obj)
+    })
+    return userArray
+}
+
+/**
+ * combines user info into the result object
+ *
+ * @return Promise - containing the user info and user results
+ */
+async function userRenderObject () {
+    let scores = await applyPercent()
+    let users = await getNameAndEmail()
+
+    let userDisplayArray = []
+    scores.forEach(function(score) {
+        users.forEach(function(user) {
+            if (score.id === user.id ) {
+                let obj = {}
+
+                obj['name'] = user.name
+                obj['email'] = user.email
+                obj['score'] = score.score
+                obj['percentage'] = score.percent
+                obj['time'] = score.time
+
+                userDisplayArray.push(obj)
+            }
+        })
+    })
+    return userDisplayArray
+}
+
+userRenderObject()

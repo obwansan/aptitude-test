ae57efb2e153beb4d541ac26e11c44156ceda7b5
Karim Kefi <karimkefi@gmail.com>
2018-04-19 10:40:23 +0100

    handleResponseFromAPI() working - just need response from API to pass in

--- app/js/sendResults.js
+++ app/js/sendResults.js
@@ -1,5 +1,35 @@
 
 
+// /**
+//  * gets all users name and email from API
+//  *
+//  * @return Array - containing the user info (Name and Email)
+//  */
+// async function submitToAPI() {
+//     let responseObj = await fetch("http://localhost:8080/answer", {
+//         method: 'post',
+//         data: {'uid':'1', 'answers': {'1': '4', '2': '3'}, 'score':'24', 'time':'29.55'}
+//         })
+//         .then(function (data) {
+//             return data.json()
+//         })
+//
+//     let booleanResponse = responseObj.success
+//     return booleanResponse
+// }
+
+
+function handleResponseFromAPI (response) {
+    let messageToTestTaker = ""
+
+    if(response.success) {
+        messageToTestTaker = 'Your results have been successfully logged'
+    } else {
+        messageToTestTaker = 'Error sending your results: Do not close browser! Contact Mike on 07812222222'
+    }
+    return messageToTestTaker
+}
+
 
 testObjSuccess = {
     "success": true,
@@ -14,10 +44,9 @@ testObjFail = {
     "data": []
 }
 
-// eRror message
-if(testObjFail.success) {
-    console.log('All ok')
-} else {
-    console.log('call Mike on 07812222222')
-}
+let response = testObjFail
+
+
+handleResponseFromAPI(response)
+
 
